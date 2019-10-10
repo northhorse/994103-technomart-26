55cde9e6325a71a71decb29ad81ddda5696e8372
northhorse <53579847+northhorse@users.noreply.github.com>
2019-10-10 12:39:49 +0300

    Пуш перед первой защитой
    Саша посмотри пожалуйста, как сможешь

--- js/item-added.js
+++ js/item-added.js
@@ -1,23 +1,27 @@
 
-var buyItem = document.querySelectorAll(".buy");
+var buyItems = document.querySelectorAll(".buy");
 var modalMessage = document.querySelector(".item-added-popup");
 var closebtn = modalMessage.querySelector(".close-button");
 
-console.log(buyItem);
 
-buyItem.addEventListener("click", function(evt){
+
+
+
+buyItems.forEach(function(item){
+  item.addEventListener("click", function(evt){
+    evt.preventDefault();
+    modalMessage.classList.add("modal-show");
+  });
+})
+
+
+
+closebtn.addEventListener("click", function(evt) {
   evt.preventDefault();
-  console.log("zarabotalo");
-
-
-  // modalMessage.classList.add("modal-show");
+  modalMessage.classList.remove("modal-show");
 
 });
 
 
 
 
-// closebtn.addEventListener("click", function(evt) {
-//   evt.preventDefault();
-//   modalMessage.classList.remove("modal-show");
-// });
--- js/write-us.js
+++ js/write-us.js
@@ -1,12 +1,13 @@
 
 var popupBtn = document.querySelector(".write-us-link");
 var popupModal = document.querySelector(".write-us-popup");
-var submitBtn = popupModal.querySelector(".submit-button");
+
 var closeModal = popupModal.querySelector(".close-button");
 var login = popupModal.querySelector("[name=for-name]");
 var mail = popupModal.querySelector("[name=e-mail]");
 var letter = popupModal.querySelector("[name=letter]");
 var form = popupModal.querySelector(".write-us-form");
+var submitBtn = form.querySelector(".submit-button");
 
 popupBtn.addEventListener("click", function(evt){
   evt.preventDefault();
@@ -14,7 +15,7 @@ popupBtn.addEventListener("click", function(evt){
 
 });
 
-console.log(login.value);
+
 
 
 submitBtn.addEventListener("click", function(evt){
@@ -23,10 +24,7 @@ submitBtn.addEventListener("click", function(evt){
 
     popupModal.classList.add("modal-error");
 
-  } else {
-    console.log("otpravka");
   }
-
 
 })
 
